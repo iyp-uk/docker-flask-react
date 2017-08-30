435e1e1b8f6af81f61bb8b801c35b9a9c9f17d71
Jerome Krieg <miaoulafrite@gmail.com>
2017-08-30 18:36:35 +0100

    Initial set up

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+# app/__init__.py
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
