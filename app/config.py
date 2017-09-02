e7d71a6db7750266b9676bde7fd724d497840da3
Jerome Krieg <miaoulafrite@gmail.com>
2017-09-02 17:57:00 +0100

    Set up postgres.

--- app/config.py
+++ app/config.py
@@ -1,8 +1,13 @@
 # app/config.py
+import os
+
+
 class BaseConfig:
     """Base configuration"""
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class DevelopmentConfig(BaseConfig):
--- manage.py
+++ manage.py
@@ -1,8 +1,17 @@
 # manage.py
 from flask_script import Manager
-from app import app
+from app import app, db
 
 manager = Manager(app)
+
+
+@manager.command
+def recreate_db():
+    """Recreates a database."""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 
 if __name__ == '__main__':
     manager.run()
